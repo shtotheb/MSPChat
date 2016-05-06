ddaf555019dad34189aa9c7a12bbf57927efb8a4
salimhbalfas <salimhbalfas@gmail.com>
2016-05-06 13:29:51 +0700

    Nulis dependencies

---
+++ app.js
@@ -0,0 +1,7 @@
+﻿var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var app = express();
